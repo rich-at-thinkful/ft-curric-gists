9279af4094a98fef70ad3961911b7b48b505a73b
Rich Greenhill <rgnet1@yahoo.com>
2017-12-05 08:52:39 -0800

    add object drills/solutions

---
+++ fundamentals/object.js
@@ -0,0 +1,153 @@
+# Object drills
+
+## 1. Object initializers and methods
+
+* Create an object called `loaf` using an object initializer (`{}`) with two properties: `flour`, which should be set to `300` and `water` which should be set to `210`.
+* Use `console.log` to print the flour and water properties.
+* Add an empty method to the loaf object called `hydration`.
+* Fill in the body of the method to return the hydration of the loaf (the water divided by the flour multiplied by 100).
+* Call the `hydration` method and use `console.log` to print the result.
+
+## 2. Iterating over an object's properties
+
+* Create an object with five properties: `foo`, `bar`, `fum`, `quux`, and `spam`. Give each property a unique value of your choosing.
+* Loop over the object using `for ... in`
+* Use `console.log` to show each property name and its associated value.
+
+## 3. Arrays in objects
+
+* Create an object with a property called `meals` which is an array of strings: `'breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'`.
+* Use `console.log` to show the name of a hobbit's fourth meal of the day.
+* Don't forget that humans and hobbits count from 1, but computers count from 0.
+
+## 4. Arrays of objects
+
+* Create 3-5 objects, each with a `name` and a `jobTitle`. Use people you know, or characters from fiction, or your own inventions.
+* Store these objects in an array.
+* Iterate over the array and use `console.log` to show each person's job title and name.
+
+## 5. Properties that aren't there
+
+* Expand on the previous example by adding a `boss` property to everyone except the owner of the company.
+* Change the iteration to print out messages in this format: `"${title} ${name} reports to ${boss}."`. For example: `Junior Engineer Bob reports to Fred.`.
+* What gets printed out for the owner?
+* Adjust the message so that people with no boss display `"${title} ${name} doesn't report to anybody."` - for example, `Founder John doesn't report to anybody.`
+
+## 6. Cracking the code
+* Redo your [Cracking the Code](https://gist.github.com/tparveen/76e0a6042fc6e0f02a2f7e0c56994715#cracking-the-code) problem from String Drills but this time use an object as your cipher. Additionally, instead of having the function accept a single word, have the function accept a single string of words, and then return the fully decoded message.
+
+## 7. Factory Functions with LOTR
+*  Write a factory function called `createCharacter` (review in [this assignment](https://courses.thinkful.com/fewd-001v6/assignment/2.6.4)) that could appropriately build characters from LOTR that have the following attributes:
+
+```
+===============================================================================================
+| Name                      | Nickname    | Race       | Origin         | Attack   | Defense  |
+-----------------------------------------------------------------------------------------------
+| Gandalf the White         | gandalf     | Wizard     | Middle Earth   | 10       | 6        |
+-----------------------------------------------------------------------------------------------
+| Bilbo Baggins             | bilbo       | Hobbit     | The Shire      | 2        | 1        |
+-----------------------------------------------------------------------------------------------
+| Frodo Baggins             | frodo       | Hobbit     | The Shire      | 3        | 2        |
+-----------------------------------------------------------------------------------------------
+| Aragorn son of Arathorn   | aragorn     | Man        | Dunnedain      | 6        | 8        |
+-----------------------------------------------------------------------------------------------
+| Legolas                   | legolas     | Elf        | Woodland Realm | 8        | 5        |
+-----------------------------------------------------------------------------------------------
+```
+
+
+* Each character should have the method `describe` which takes no parameters and prints out the string: `"{name} is a {race} from {origin}."`
+
+* Each character should also have a method called `evaluateFight` that takes in a `character` object and returns the following string: `"Your opponent takes {x} damage and you receive {y} damage"` where `x` and `y` are the differences between each characters `attack` and `defense` values.  If `defense` exceeds `attack`, then take zero damage.
+
+* Using array literal syntax, create an array `characters` that calls your factory function for each character in the table above with the relevant parameters.  Your `characters` array should now have 5 objects in it.
+
+* Add a new character to `characters` (make up any attributes not provided):
+  * Arwen Undomiel is a Half-Elf of Rivendell 
+  
+* Using the `.find()` function, retrieve your character nicknamed `aragorn` from `characters` and then call his `describe` method.
+
+* Using the `.filter()` function, create a new array from `characters` that ONLY contains characters of the race `Hobbit`.
+
+* Using the `.filter()` function, create a new array from `characters` that ONLY contains characters with attack value above 5.
+
+* What if you wanted to equip a weapon for each character and change how they are described? For example:
+  * Gandolf the White is a Wizard of the Middle Earth who uses a wizard staff
+  * Bilbo Baggings is a Hobbit of the Shire who uses the Ring
+  * Frodo ... String and Barrow Blade
+  * Aragon .... Anduril
+  * Legolas ... Bow and Arrow
+  * Arwen .... Hadhafang
+
+  How would you change the factory function and other methods?
+  
+## 8. BONUS: A Database Search
+
+Put this dataset at the top of your program:
+
+```javascript
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+```
+
+Write a function `findOne()` that takes in the following two arguments:
+
+1. `arr` - array of Avenger objects to search through
+2. `query` - object with one or more key/value pairs that must exactly match the target Avenger
+
+Only one result should be returned even if multiple match. If a match isn't found, return `null`
+
+With the above data set, you should be able to call `findOne` with the arguments shown below and return the following results:
+
+```javascript
+findOne(HEROES, { id: 1 });
+=> { id: 1, name: 'Captain America', squad: 'Avengers' }
+
+findOne(HEROES, { id: 10 });
+=> null
+
+findOne(HEROES, { id: 2, name: 'Aquaman' });
+=> null
+
+findOne(HEROES, { id: 5, squad: 'Justice League' });
+=> { id: 5, name: 'Wonder Woman', squad: 'Justice League' }
+
+findOne(HEROES, { squad: 'Justice League' });
+=> { id: 4, name: 'Superman', squad: 'Justice League' }
+```
+
+## 8a. BONUS II: A Database Method
+
+Let's create a fake database in memory with the same dataset:
+
+```javascript
+const Database = {
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' },
+    ]
+  }
+};
+```
+
+Add a method to `Database` called `findOne` and have it behave the same as the `findOne` function above. However, instead of referencing a `HEROES` array in the global scope, it should pull from the `store` in the `Database`.  HINT: You'll want to use `this` for this...
+
+The usage should be:
+
+```javascript
+Database.findOne({ id: 2 });
+=> { id: 2, name: 'Iron Man', squad: 'Avengers' }
+```
---
+++ fundamentals/solutions/objects-solutions.md
@@ -0,0 +1,216 @@
+Example solutions to [today's object drills](https://gist.github.com/MrSkinny/41a75fc08a402358384226ec22fdd0de).
+This is not the sole right way to solve any of these problems.
+
+## 1. Object Initializers and Methods
+
+```javascript
+const loaf = {
+    flour: 300,
+    water: 210
+};
+console.log("Flour:", loaf.flour);
+console.log("Water:", loaf.water);
+```
+
+```javascript
+const loaf = {
+    flour: 300,
+    water: 210,
+    hydration: function() {}
+};
+```
+
+```javascript
+const loaf = {
+    flour: 300,
+    water: 210,
+    hydration: function() {
+        return this.water / this.flour * 100;
+    }
+};
+console.log("Hydration:", loaf.hydration());
+```
+
+## 2. Iterating over an object's properties
+
+```javascript
+const meta = {
+    foo: "scram",
+    bar: "whiskey",
+    fum: "giant",
+    quux: "steele",
+    spam: "van rossum"
+};
+for (let name in meta) {
+    console.log(name, meta[name]);
+}
+```
+
+## 3. Arrays in objects
+
+```javascript
+const hobbit = {meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']};
+console.log(hobbit.meals[3]);
+```
+
+## 4. Arrays of objects
+
+```javascript
+const staff = [
+    {name: "Roy", job_title: "Off-And-On Again Guy"},
+    {name: "Moss", job_title: "Nerd"},
+    {name: "Jen", job_title: "IT manager"},
+    {name: "Reynholm", job_title: "Large Ham"},
+];
+for (let i=0; i<staff.length; i++) {
+    const employee = staff[i];
+    console.log(employee.name, "is the", employee.job_title);
+}
+```
+
+## 5. Properties that aren't there
+
+```javascript
+const staff = [
+    {name: "Roy", job_title: "Off-And-On Again Guy", boss: "Jen"},
+    {name: "Moss", job_title: "Nerd", boss: "Jen"},
+    {name: "Jen", job_title: "IT manager", boss: "Reynholm"},
+    {name: "Reynholm", job_title: "Large Ham"},
+];
+for (let i=0; i<staff.length; i++) {
+    const employee = staff[i];
+    console.log(employee.job_title, employee.name, "reports to", employee.boss);
+}
+```
+
+* The owner is shown as "reports to undefined", because looking up a property that doesn't exist yields the special value `undefined`.
+
+```javascript
+for (let i=0; i<staff.length; i++) {
+    const employee = staff[i];
+    if (!employee.boss) {
+        console.log(employee.job_title, employee.name, "doesn't report to anybody.");
+    }
+    else {
+        console.log(employee.job_title, employee.name, "reports to", employee.boss);
+    }
+}
+```
+
+## 6. Cracking the Code
+
+```javascript
+function decode(words){
+    const cipher = {
+        a: 2,
+        b: 3,
+        c: 4,
+        d: 5  
+    };
+    
+    const wordsArray = words.split(' ');
+    const decodedChars = wordsArray.map(word => {
+        let offset;
+        if (cipher[word[0]]) {
+            offset = cipher[word[0]] - 1
+        };
+
+        return offset ? word[offset] : ' ';
+    });
+    
+    return decodedChars.join('');
+}
+```
+
+## 7. Factory Functions with LOTR
+
+```javascript
+// Create the factory function:
+
+function createCharacter(name, nickname, race, origin, atk, def){
+  return {
+    name, nickname, race, origin, atk, def,
+
+    describe() {
+      console.log(`${name} is a ${race} from ${origin}.`);
+    },
+    
+    evaluateFight(character) {
+      let dmgDealt = 0, dmgReceived = 0;
+      
+      if (this.atk > character.def) {
+        dmgDealt = this.atk - character.def
+      }
+      
+      if (this.def < character.atk) {
+        dmgReceived = character.atk - this.def;
+      }
+      
+      console.log(`Your opponent takes ${dmgDealt} damage and you receive ${dmgReceived} damage.`);
+    }
+  };
+}
+
+// Create array of characters:
+
+const characters = [
+  createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6),
+  createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1),
+  createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+  createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunedain', 6, 8),
+  createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5)
+];
+
+// Add character to array:
+
+characters.push(createCharacter('Arwen Undomiel', 'arwen', 'Half-elf', 'Rivendell', 3, 5));
+
+// Find character and call its `describe` method
+
+characters.find(char => char.nickname === 'aragorn').describe();
+
+// Create array of only hobbits:
+
+const onlyHobbits = characters.filter(char => char.race === 'Hobbit');
+console.log(onlyHobbits);
+
+// Create array of only high attack characters:
+
+const onlyHighAttack = characters.filter(char => char.atk > 5);
+console.log(onlyHighAttack);
+```
+
+## 8. Database Search
+
+This solution aims to perform the **least** iterations necessary for best performance, exiting a loop at earliest known moment where further searching is not necessary. It implements the [`continue`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/continue) and [`break`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/break) keywords.
+
+```javascript
+const findOne = function(arr, query) {
+  
+  // See for-of loops - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
+  // Iterates through array placing current element in locally scoped variable `hero`
+  for (const hero of arr) {
+    let assumeMatch = true;
+
+    // loop through each key in `query` object
+    for (const key in query) {
+      
+      // when first value in query[key] does NOT match hero[key]
+      // break out of the loop and set assumeMatch = false
+      if (query[key] !== hero[key]) {
+        assumeMatch = false;
+        break;
+      }
+    }
+    
+    // if failed to match, continue to next hero or exit loop if at last hero
+    if (!assumeMatch) continue;
+    
+    // otherwise a match was found! return the hero
+    return hero;
+  }
+
+  // heroes array was exited without a match so return null
+  return null;
+};
+```
